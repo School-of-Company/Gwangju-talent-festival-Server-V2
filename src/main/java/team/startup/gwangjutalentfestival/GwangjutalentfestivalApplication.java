333aca013db118e8fc48c7ceb5f36ec617b2fc35
C0ng_yun <s25069@gsm.hs.kr>
2026-02-25 16:15:31 +0900

    init

---
+++ src/main/java/team/startup/gwangjutalentfestival/GwangjutalentfestivalApplication.java
@@ -0,0 +1,13 @@
+package team.startup.gwangjutalentfestival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwangjutalentfestivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwangjutalentfestivalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/startup/gwangjutalentfestival/GwangjutalentfestivalApplicationTests.java
@@ -0,0 +1,13 @@
+package team.startup.gwangjutalentfestival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwangjutalentfestivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
