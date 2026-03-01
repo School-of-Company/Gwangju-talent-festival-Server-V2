dc56f5a93e6197f1f8c00a9a7ca6ce653fa5c09e
C0ng_yun <s25069@gsm.hs.kr>
2026-03-01 19:05:12 +0900

    add :: JwtFilter 추가

---
+++ src/main/java/team/startup/gwangjutalentfestival/global/security/filter/JwtFilter.java
@@ -0,0 +1,70 @@
+package team.startup.gwangjutalentfestival.global.security.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import team.startup.gwangjutalentfestival.domain.user.enums.Role;
+import team.startup.gwangjutalentfestival.global.auth.CustomUserDetails;
+import team.startup.gwangjutalentfestival.global.exception.ErrorCode;
+import team.startup.gwangjutalentfestival.global.jwt.JwtProvider;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtProvider.resolveToken(request);
+
+        if (token == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if (!jwtProvider.validateToken(token)) {
+            setErrorResponse(response, ErrorCode.INVALID_TOKEN);
+        }
+
+        if (!jwtProvider.isAccessToken(token)) {
+            setErrorResponse(response, ErrorCode.INVALID_TOKEN);
+        }
+
+        Long userId = jwtProvider.getUserId(token);
+        Role role = jwtProvider.getRole(token);
+        String phoneNumber = jwtProvider.getPhoneNumber(token);
+
+        CustomUserDetails customUserDetails = CustomUserDetails.fromToken(userId, token, role);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                customUserDetails,
+                null,
+                customUserDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+    }
+
+
+    private void setErrorResponse(HttpServletResponse response, ErrorCode errorCode) throws IOException {
+        response.setStatus(errorCode.getStatus());
+        response.setContentType("application/json; charset=UTF-8");
+        response.getWriter().write(
+                "{\"status\":" + errorCode.getStatus() + ",\"message\":\"" + errorCode.getMessage() + "\"}"
+        );
+    }
+}
