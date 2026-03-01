cba4d0206b38fd99dcf7a48ce9c860a660e308dd
C0ng_yun <s25069@gsm.hs.kr>
2026-03-01 19:06:38 +0900

    add :: UserEntity 추가

---
+++ src/main/java/team/startup/gwangjutalentfestival/domain/user/entity/UserEntity.java
@@ -0,0 +1,32 @@
+package team.startup.gwangjutalentfestival.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.startup.gwangjutalentfestival.domain.user.enums.Role;
+
+
+@Entity
+@Getter
+@Table(name = "user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true,nullable = true,name = "phone_number")
+    private String phoneNumber;
+
+    @Column(unique = true,nullable = true,name = "email")
+    private String email;
+
+    @Column(nullable = true,name = "password")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false,name = "role")
+    private Role role;
+}
