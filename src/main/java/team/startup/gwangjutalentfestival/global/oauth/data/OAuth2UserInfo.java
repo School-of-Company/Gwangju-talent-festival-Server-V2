79a9f1f911c1ea03b8c4fb86dc62ed64ec996789
C0ng_yun <s25069@gsm.hs.kr>
2026-03-06 11:07:38 +0900

    add :: OAuth2UserInfo 추가

---
+++ src/main/java/team/startup/gwangjutalentfestival/global/oauth/data/OAuth2UserInfo.java
@@ -0,0 +1,7 @@
+package team.startup.gwangjutalentfestival.global.oauth.data;
+
+import team.startup.gwangjutalentfestival.global.oauth.dto.OAuthUserResponse;
+
+public interface OAuth2UserInfo {
+    OAuthUserResponse toResponse();
+}
