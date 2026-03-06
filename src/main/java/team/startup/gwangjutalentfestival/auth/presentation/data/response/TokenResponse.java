ff8144faaa9db942ed58b4f1d0c2d3569a958ede
C0ng_yun <s25069@gsm.hs.kr>
2026-03-06 11:51:42 +0900

    update :: 필드명 변경

--- src/main/java/team/startup/gwangjutalentfestival/auth/presentation/data/response/TokenResponse.java
+++ src/main/java/team/startup/gwangjutalentfestival/auth/presentation/data/response/TokenResponse.java
@@ -8,10 +8,10 @@ import java.time.LocalDateTime;
 public record TokenResponse(
         String accessToken,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-        LocalDateTime accessTokenExpiresIn,
+        LocalDateTime accessTokenExpiresAt,
         String refreshToken,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-        LocalDateTime refreshTokenExpiresIn,
+        LocalDateTime refreshTokenExpiresAt,
         Role role
 ) {
 }
