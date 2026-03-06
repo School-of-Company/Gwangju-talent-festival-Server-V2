dfa3e8e024f0c10ef97b4c10c0b39a28de5aa741
C0ng_yun <s25069@gsm.hs.kr>
2026-03-06 12:00:31 +0900

    update :: 분기처리로 변경

--- src/main/java/team/startup/gwangjutalentfestival/global/oauth/factory/OAuth2UserInfoFactory.java
+++ src/main/java/team/startup/gwangjutalentfestival/global/oauth/factory/OAuth2UserInfoFactory.java
@@ -8,6 +8,9 @@ import java.util.Map;
 public class OAuth2UserInfoFactory {
 
     public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
-        return new KakaoOAuth2UserInfo(attributes);
+        if (team.startup.gwangjutalentfestival.global.oauth.common.OAuthType.KAKAO.name().equalsIgnoreCase(registrationId)) {
+            return new KakaoOAuth2UserInfo(attributes);
+        }
+        throw new IllegalArgumentException("지원하지 않는 로그인 방식입니다: " + registrationId);
     }
 }
