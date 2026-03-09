afb571a0d3787ff7a46eac3c6fe6b0e5310ace48
C0ng_yun <s25069@gsm.hs.kr>
2026-03-09 19:21:27 +0900

    add :: FeignConfig 추가

---
+++ src/main/java/team/startup/gwangjutalentfestival/global/config/FeignConfig.java
@@ -0,0 +1,13 @@
+package team.startup.gwangjutalentfestival.global.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+}
