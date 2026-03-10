4cc87f536ac6e2da2f77a9a972a71601a887defe
C0ng_yun <s25069@gsm.hs.kr>
2026-03-10 14:00:54 +0900

    update :: 예외처리 클래스 이름 에러코드와 통일

--- src/main/java/team/startup/gwangjutalentfestival/global/oauth/exception/OAuthProviderNotSupportedException.java
+++ src/main/java/team/startup/gwangjutalentfestival/global/oauth/exception/OAuthProviderNotSupportedException.java
@@ -3,8 +3,8 @@ package team.startup.gwangjutalentfestival.global.oauth.exception;
 import team.startup.gwangjutalentfestival.global.exception.ErrorCode;
 import team.startup.gwangjutalentfestival.global.exception.GlobalException;
 
-public class UnsupportedOAuthProviderException extends GlobalException {
-    public UnsupportedOAuthProviderException() {
+public class OAuthProviderNotSupportedException extends GlobalException {
+    public OAuthProviderNotSupportedException() {
         super(ErrorCode.OAUTH_PROVIDER_NOT_SUPPORTED);
     }
 }
--- src/main/java/team/startup/gwangjutalentfestival/global/oauth/factory/OAuth2UserInfoFactory.java
+++ src/main/java/team/startup/gwangjutalentfestival/global/oauth/factory/OAuth2UserInfoFactory.java
@@ -1,17 +1,18 @@
 package team.startup.gwangjutalentfestival.global.oauth.factory;
 
+import team.startup.gwangjutalentfestival.global.oauth.common.OAuthType;
 import team.startup.gwangjutalentfestival.global.oauth.data.KakaoOAuth2UserInfo;
 import team.startup.gwangjutalentfestival.global.oauth.data.OAuth2UserInfo;
-import team.startup.gwangjutalentfestival.global.oauth.exception.UnsupportedOAuthProviderException;
+import team.startup.gwangjutalentfestival.global.oauth.exception.OAuthProviderNotSupportedException;
 
 import java.util.Map;
 
 public class OAuth2UserInfoFactory {
 
     public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
-        if (team.startup.gwangjutalentfestival.global.oauth.common.OAuthType.KAKAO.name().equalsIgnoreCase(registrationId)) {
+        if (OAuthType.KAKAO.name().equalsIgnoreCase(registrationId)) {
             return new KakaoOAuth2UserInfo(attributes);
         }
-        throw new UnsupportedOAuthProviderException();
+        throw new OAuthProviderNotSupportedException();
     }
 }
