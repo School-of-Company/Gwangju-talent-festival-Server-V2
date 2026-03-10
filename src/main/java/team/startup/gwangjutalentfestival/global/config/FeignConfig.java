d3d1db5c4a8c1b3bb1d45c8b0786cad267e28379
C0ng_yun <s25069@gsm.hs.kr>
2026-03-10 13:21:56 +0900

    update :: FULL -> BASIC 변경

--- src/main/java/team/startup/gwangjutalentfestival/global/config/FeignConfig.java
+++ src/main/java/team/startup/gwangjutalentfestival/global/config/FeignConfig.java
@@ -1,13 +1,18 @@
 package team.startup.gwangjutalentfestival.global.config;
 
 import feign.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
+
+    @Value("${feign.log-level:BASIC}")
+    private String logLevel;
+
     @Bean
     public Logger.Level feignLoggerLevel() {
-        return Logger.Level.FULL;
+        return Logger.Level.valueOf(logLevel);
     }
 }
