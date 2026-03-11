2eeb71353a2d10eccddd19d886cdaa1f7c0912fc
C0ng_yun <s25069@gsm.hs.kr>
2026-03-11 13:13:00 +0900

    delete :: OAuthConfiguration 허용 삭제

--- src/main/java/team/startup/gwangjutalentfestival/GwangjutalentfestivalApplication.java
+++ src/main/java/team/startup/gwangjutalentfestival/GwangjutalentfestivalApplication.java
@@ -2,17 +2,14 @@ package team.startup.gwangjutalentfestival;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import team.startup.gwangjutalentfestival.global.jwt.JwtProperties;
 import team.startup.gwangjutalentfestival.global.security.properties.CorsProperties;
 
 @EnableFeignClients
-@EnableConfigurationProperties({JwtProperties.class,CorsProperties.class})
-@SpringBootApplication(exclude = {
-        OAuth2ClientAutoConfiguration.class
-})
+@EnableConfigurationProperties({JwtProperties.class, CorsProperties.class})
+@SpringBootApplication
 public class GwangjutalentfestivalApplication {
 
 	public static void main(String[] args) {
