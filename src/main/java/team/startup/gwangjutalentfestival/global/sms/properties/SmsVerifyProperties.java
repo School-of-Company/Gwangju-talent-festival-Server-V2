70e72140924213c65f93c592dc74e969db7c0247
C0ng_yun <s25069@gsm.hs.kr>
2026-03-13 08:06:47 +0900

    add :: SmsVerifyProperties 추가

---
+++ src/main/java/team/startup/gwangjutalentfestival/global/sms/properties/SmsVerifyProperties.java
@@ -0,0 +1,14 @@
+package team.startup.gwangjutalentfestival.global.sms.properties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "sms.verify")
+public class SmsVerifyProperties {
+    private int maxSendCount;
+    private long verifyCodeTtl;
+    private Integer verifyCountTtl;
+}
