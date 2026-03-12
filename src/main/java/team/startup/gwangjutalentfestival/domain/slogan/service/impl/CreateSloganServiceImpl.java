3b9b2a5799c9a1ff282edda6b5e36c41e177ad27
박하민 <hamin090403@gmail.com>
2026-03-13 08:27:27 +0900

    add :: validateSloganSubmissionPeriod 메서드 추가

--- src/main/java/team/startup/gwangjutalentfestival/domain/slogan/service/impl/CreateSloganServiceImpl.java
+++ src/main/java/team/startup/gwangjutalentfestival/domain/slogan/service/impl/CreateSloganServiceImpl.java
@@ -6,9 +6,14 @@ import org.springframework.transaction.annotation.Transactional;
 import team.startup.gwangjutalentfestival.domain.auth.exception.DuplicatePhoneNumberException;
 import team.startup.gwangjutalentfestival.domain.slogan.dto.request.CreateSloganRequest;
 import team.startup.gwangjutalentfestival.domain.slogan.entity.SloganEntity;
+import team.startup.gwangjutalentfestival.domain.slogan.exception.SloganSubmissionPeriodException;
+import team.startup.gwangjutalentfestival.domain.slogan.properties.SloganSubmissionProperties;
 import team.startup.gwangjutalentfestival.domain.slogan.repository.SloganRepository;
 import team.startup.gwangjutalentfestival.domain.slogan.service.CreateSloganService;
 import team.startup.gwangjutalentfestival.global.thirdparty.google.adapter.GoogleSheetsAdapter;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Service
 @RequiredArgsConstructor
@@ -16,10 +21,12 @@ public class CreateSloganServiceImpl implements CreateSloganService {
 
     private final SloganRepository sloganRepository;
     private final GoogleSheetsAdapter googleSheetsAdapter;
+    private final SloganSubmissionProperties sloganSubmissionProperties;
 
     @Override
     @Transactional
     public void execute(CreateSloganRequest request) {
+        validateSloganSubmissionPeriod();
         validateDuplicatePhoneNumber(request.phoneNumber());
 
         SloganEntity slogan = SloganEntity.builder()
@@ -39,6 +46,15 @@ public class CreateSloganServiceImpl implements CreateSloganService {
     private void validateDuplicatePhoneNumber(String phoneNumber) {
         if (sloganRepository.existsByPhoneNumber(phoneNumber)) {
             throw new DuplicatePhoneNumberException();
+        }
+    }
+
+    private void validateSloganSubmissionPeriod() {
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+
+        if (now.isBefore(sloganSubmissionProperties.startAt()) ||
+                now.isAfter(sloganSubmissionProperties.endAt())) {
+            throw new SloganSubmissionPeriodException();
         }
     }
 }
