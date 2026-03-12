2bb52b5a9fbd60ac856e282fc9d0529e11c43a1e
C0ng_yun <s25069@gsm.hs.kr>
2026-03-13 08:11:54 +0900

    update :: @Repository 추가

--- src/main/java/team/startup/gwangjutalentfestival/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/team/startup/gwangjutalentfestival/domain/auth/repository/RefreshTokenRepository.java
@@ -1,7 +1,9 @@
 package team.startup.gwangjutalentfestival.domain.auth.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import team.startup.gwangjutalentfestival.domain.auth.entity.RefreshToken;
 
+@Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 }
