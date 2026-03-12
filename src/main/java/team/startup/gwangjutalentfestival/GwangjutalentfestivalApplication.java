cfed8fef97ecc8a557b455dfa37787d0cfd65ab3
C0ng_yun <s25069@gsm.hs.kr>
2026-03-13 08:12:10 +0900

    update :: SolapiProperties 허용

--- src/main/java/team/startup/gwangjutalentfestival/GwangjutalentfestivalApplication.java
+++ src/main/java/team/startup/gwangjutalentfestival/GwangjutalentfestivalApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import team.startup.gwangjutalentfestival.global.jwt.JwtProperties;
 import team.startup.gwangjutalentfestival.global.security.properties.CorsProperties;
+import team.startup.gwangjutalentfestival.global.sms.properties.SmsVerifyProperties;
+import team.startup.gwangjutalentfestival.global.sms.properties.SolapiProperties;
 
 @EnableFeignClients
-@EnableConfigurationProperties({JwtProperties.class, CorsProperties.class})
+@EnableConfigurationProperties({JwtProperties.class, CorsProperties.class, SolapiProperties.class, SmsVerifyProperties.class})
 @SpringBootApplication
 public class GwangjutalentfestivalApplication {
 
