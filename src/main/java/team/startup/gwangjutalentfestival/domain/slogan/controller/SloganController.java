7c6695ff2a76cc5fcb06f9ea84c58ae0ce9a79e4
박하민 <hamin090403@gmail.com>
2026-03-12 15:57:21 +0900

    add :: SloganController 추가

---
+++ src/main/java/team/startup/gwangjutalentfestival/domain/slogan/controller/SloganController.java
@@ -0,0 +1,27 @@
+package team.startup.gwangjutalentfestival.domain.slogan.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team.startup.gwangjutalentfestival.domain.slogan.dto.request.CreateSloganRequest;
+import team.startup.gwangjutalentfestival.domain.slogan.entity.Slogan;
+import team.startup.gwangjutalentfestival.domain.slogan.service.CreateSloganService;
+
+@RestController
+@RequestMapping("/slogan")
+@RequiredArgsConstructor
+public class SloganController {
+
+    private final CreateSloganService createSloganService;
+
+    @PostMapping
+    public ResponseEntity<Slogan> createSlogan(@Valid @RequestBody CreateSloganRequest request) {
+        createSloganService.execute(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
