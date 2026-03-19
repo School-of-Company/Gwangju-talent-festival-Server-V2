7b706b2a209e09857df1f5c60e6b2dfcd468a0dc
C0ng_yun <s25069@gsm.hs.kr>
2026-03-19 20:28:37 +0900

    add :: SeatEntity 추가

---
+++ src/main/java/team/startup/gwangjutalentfestival/domain/seat/entity/SeatEntity.java
@@ -0,0 +1,43 @@
+package team.startup.gwangjutalentfestival.domain.seat.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import team.startup.gwangjutalentfestival.domain.team.entity.TeamEntity;
+import team.startup.gwangjutalentfestival.domain.user.entity.UserEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(
+        name = "seat",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"seat_section", "seat_number"})
+        }
+)
+public class SeatEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "seat_section")
+    private Character seatSection;
+
+    @Column(nullable = false,name = "seat_number")
+    private Integer seatNumber;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tema_id")
+    private TeamEntity team;
+}
