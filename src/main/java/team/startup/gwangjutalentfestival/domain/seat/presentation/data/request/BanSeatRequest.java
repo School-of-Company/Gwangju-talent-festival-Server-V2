2027d8c3de2ac1546d4405629b34ed30f3d1566d
박하민 <hamin090403@gmail.com>
2026-03-24 21:51:57 +0900

    add :: BanSeatService 구현

---
+++ src/main/java/team/startup/gwangjutalentfestival/domain/seat/presentation/data/request/BanSeatRequest.java
@@ -0,0 +1,21 @@
+package team.startup.gwangjutalentfestival.domain.seat.presentation.data.request;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
+import team.startup.gwangjutalentfestival.domain.user.enums.Role;
+
+public record BanSeatRequest(
+        @Pattern(
+                regexp = "^[A-J]$",
+                message = "좌석 섹션은 A부터 J까지 존재합니다."
+        )
+        String seatSection,
+
+        @Min(value = 1, message = "좌석 번호는 최소 1번부터 존재합니다.")
+        @Max(value = 154, message = "좌석 번호는 최대 154번까지 존재합니다.")
+        Integer seatNumber,
+
+        Role role
+) {
+}
