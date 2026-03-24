3b4a8f0fd9b42c6c5b0293cce2e25beb1d8bb0c3
C0ng_yun <s25069@gsm.hs.kr>
2026-03-25 08:13:42 +0900

    update :: 사용하지 않는 import문 삭제

--- src/main/java/team/startup/gwangjutalentfestival/domain/team/repository/TeamRepositoryCustomImpl.java
+++ src/main/java/team/startup/gwangjutalentfestival/domain/team/repository/TeamRepositoryCustomImpl.java
@@ -1,6 +1,5 @@
 package team.startup.gwangjutalentfestival.domain.team.repository;
 
-import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
