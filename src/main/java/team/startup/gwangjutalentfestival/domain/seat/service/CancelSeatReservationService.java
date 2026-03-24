6ab33471bf8fa3cbff4e8e45c447a5e353b53dfe
박하민 <hamin090403@gmail.com>
2026-03-24 21:50:58 +0900

    add :: CancelSeatReservationService 구현

---
+++ src/main/java/team/startup/gwangjutalentfestival/domain/seat/service/CancelSeatReservationService.java
@@ -0,0 +1,5 @@
+package team.startup.gwangjutalentfestival.domain.seat.service;
+
+public interface CancelSeatReservationService {
+    void execute();
+}
---
+++ src/main/java/team/startup/gwangjutalentfestival/domain/seat/service/impl/CancelSeatReservationServiceImpl.java
@@ -0,0 +1,33 @@
+package team.startup.gwangjutalentfestival.domain.seat.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.startup.gwangjutalentfestival.domain.seat.entity.SeatEntity;
+import team.startup.gwangjutalentfestival.domain.seat.exception.SeatNotFoundException;
+import team.startup.gwangjutalentfestival.domain.seat.repository.SeatReservationRepository;
+import team.startup.gwangjutalentfestival.domain.seat.service.CancelSeatReservationService;
+import team.startup.gwangjutalentfestival.domain.user.entity.UserEntity;
+import team.startup.gwangjutalentfestival.domain.user.exception.UserNotFoundException;
+import team.startup.gwangjutalentfestival.domain.user.repository.UserRepository;
+import team.startup.gwangjutalentfestival.global.util.UserUtil;
+
+@Service
+@RequiredArgsConstructor
+public class CancelSeatReservationServiceImpl implements CancelSeatReservationService {
+
+    private final UserRepository userRepository;
+    private final SeatReservationRepository seatReservationRepository;
+
+    @Override
+    @Transactional
+    public void execute() {
+        UserEntity user = userRepository.findById(UserUtil.getCurrentUserId())
+                .orElseThrow(UserNotFoundException::new);
+
+        SeatEntity seat = seatReservationRepository.findByUser(user)
+                .orElseThrow(SeatNotFoundException::new);
+
+        seatReservationRepository.delete(seat);
+    }
+}
