49715c03e2a2c653f5624b5c08cd5aa7eafdbe7b
박하민 <hamin090403@gmail.com>
2026-03-24 21:52:06 +0900

    add :: CancelSeatBanService 구현

---
+++ src/main/java/team/startup/gwangjutalentfestival/domain/seat/service/admin/impl/CancelSeatBanServiceImpl.java
@@ -0,0 +1,32 @@
+package team.startup.gwangjutalentfestival.domain.seat.service.admin.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.startup.gwangjutalentfestival.domain.seat.entity.SeatBanEntity;
+import team.startup.gwangjutalentfestival.domain.seat.exception.SeatBanNotFoundException;
+import team.startup.gwangjutalentfestival.domain.seat.exception.SeatNotFoundException;
+import team.startup.gwangjutalentfestival.domain.seat.presentation.data.request.CancelSeatBanRequest;
+import team.startup.gwangjutalentfestival.domain.seat.repository.SeatBanRepository;
+import team.startup.gwangjutalentfestival.domain.seat.repository.SeatReservationRepository;
+import team.startup.gwangjutalentfestival.domain.seat.service.admin.CancelSeatBanService;
+
+@Service
+@RequiredArgsConstructor
+public class CancelSeatBanServiceImpl implements CancelSeatBanService {
+
+    private final SeatBanRepository seatBanRepository;
+    private final SeatReservationRepository seatReservationRepository;
+
+    @Override
+    @Transactional
+    public void execute(CancelSeatBanRequest request) {
+        Character seatSection = request.seatSection().charAt(0);
+
+        SeatBanEntity seatBan = seatBanRepository
+                .findBySeatSectionAndSeatNumber(seatSection, request.seatNumber())
+                .orElseThrow(SeatBanNotFoundException::new);
+
+        seatBanRepository.delete(seatBan);
+    }
+}
