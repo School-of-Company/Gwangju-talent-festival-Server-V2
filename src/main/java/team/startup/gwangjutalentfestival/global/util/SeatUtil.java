3938eefd8f38814a8b7b67850aad6d16d7b97bff
박하민 <hamin090403@gmail.com>
2026-03-24 21:53:47 +0900

    add :: SeatUtil 추가

---
+++ src/main/java/team/startup/gwangjutalentfestival/global/util/SeatUtil.java
@@ -0,0 +1,21 @@
+package team.startup.gwangjutalentfestival.global.util;
+
+import org.springframework.stereotype.Component;
+import team.startup.gwangjutalentfestival.domain.seat.exception.SeatNotFoundException;
+
+import java.util.Map;
+
+@Component
+public class SeatUtil {
+    private static final Map<Character, Integer> SEAT_MAP = Map.of(
+            'A', 77, 'B', 130, 'C', 154, 'D', 130, 'E', 77,
+            'F', 54, 'G', 100, 'H', 119, 'I', 100, 'J', 54
+    );
+
+    public Integer getMaxSeats(Character section) {
+        if (!SEAT_MAP.containsKey(section)) {
+            throw new SeatNotFoundException();
+        }
+        return SEAT_MAP.get(section);
+    }
+}
