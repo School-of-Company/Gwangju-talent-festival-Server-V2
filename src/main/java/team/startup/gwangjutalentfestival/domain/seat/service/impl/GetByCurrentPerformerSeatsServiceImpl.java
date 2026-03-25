d519d9d1befa2196e2f6449c15a74f25fbd5d10e
박하민 <hamin090403@gmail.com>
2026-03-26 01:23:19 +0900

    add :: GetByCurrentPerformerSeatsService 구현

---
+++ src/main/java/team/startup/gwangjutalentfestival/domain/seat/service/impl/GetByCurrentPerformerSeatsServiceImpl.java
@@ -0,0 +1,32 @@
+package team.startup.gwangjutalentfestival.domain.seat.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.startup.gwangjutalentfestival.domain.seat.entity.SeatEntity;
+import team.startup.gwangjutalentfestival.domain.seat.presentation.data.response.GetSeatResponse;
+import team.startup.gwangjutalentfestival.domain.seat.repository.SeatReservationRepository;
+import team.startup.gwangjutalentfestival.domain.seat.service.GetByCurrentPerformerSeatsService;
+import team.startup.gwangjutalentfestival.global.util.UserUtil;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GetByCurrentPerformerSeatsServiceImpl implements GetByCurrentPerformerSeatsService {
+
+    private final SeatReservationRepository seatReservationRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<GetSeatResponse> execute() {
+        List<SeatEntity> seats = seatReservationRepository.findAllByUserId(UserUtil.getCurrentUserId());
+
+        return seats.stream()
+                .map(seat -> new GetSeatResponse(
+                        seat.getSeatSection().toString(),
+                        seat.getSeatNumber()
+                ))
+                .toList();
+    }
+}
