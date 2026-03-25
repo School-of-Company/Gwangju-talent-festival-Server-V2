a84d7a47196e31e2bef7bab43c55f96215bd2d19
박하민 <hamin090403@gmail.com>
2026-03-26 01:23:32 +0900

    add :: GetCurrentUserSeatService 구현

---
+++ src/main/java/team/startup/gwangjutalentfestival/domain/seat/service/GetCurrentUserSeatService.java
@@ -0,0 +1,7 @@
+package team.startup.gwangjutalentfestival.domain.seat.service;
+
+import team.startup.gwangjutalentfestival.domain.seat.presentation.data.response.GetSeatResponse;
+
+public interface GetCurrentUserSeatService {
+    GetSeatResponse execute();
+}
---
+++ src/main/java/team/startup/gwangjutalentfestival/domain/seat/service/impl/GetCurrentUserSeatServiceImpl.java
@@ -0,0 +1,34 @@
+package team.startup.gwangjutalentfestival.domain.seat.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.startup.gwangjutalentfestival.domain.seat.entity.SeatEntity;
+import team.startup.gwangjutalentfestival.domain.seat.exception.SeatNotFoundException;
+import team.startup.gwangjutalentfestival.domain.seat.presentation.data.response.GetSeatResponse;
+import team.startup.gwangjutalentfestival.domain.seat.repository.SeatReservationRepository;
+import team.startup.gwangjutalentfestival.domain.seat.service.GetCurrentUserSeatService;
+import team.startup.gwangjutalentfestival.domain.user.entity.UserEntity;
+import team.startup.gwangjutalentfestival.global.util.UserUtil;
+
+@Service
+@RequiredArgsConstructor
+public class GetCurrentUserSeatServiceImpl implements GetCurrentUserSeatService {
+
+    private final SeatReservationRepository seatReservationRepository;
+    private final UserUtil userUtil;
+
+    @Override
+    @Transactional(readOnly = true)
+    public GetSeatResponse execute() {
+        UserEntity user = userUtil.getCurrentUser();
+
+        SeatEntity seat = seatReservationRepository.findByUser(user)
+                .orElseThrow(SeatNotFoundException::new);
+
+        return new GetSeatResponse(
+                seat.getSeatSection().toString(),
+                seat.getSeatNumber()
+        );
+    }
+}
