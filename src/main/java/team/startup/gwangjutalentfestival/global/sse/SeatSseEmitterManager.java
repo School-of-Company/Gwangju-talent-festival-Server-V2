d660c63bc427a50df17a3e4cbe473d065fb35acc
박하민 <hamin090403@gmail.com>
2026-03-26 01:27:19 +0900

    add :: SeatSseEmitterManager 추가

---
+++ src/main/java/team/startup/gwangjutalentfestival/global/sse/SeatSseEmitterManager.java
@@ -0,0 +1,42 @@
+package team.startup.gwangjutalentfestival.global.sse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class SeatSseEmitterManager {
+    private static final long SSE_TIMEOUT_MILLIS = 60 * 60 * 1000L;
+
+    private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
+
+    public SseEmitter addEmitter(String phoneNumber) {
+        SseEmitter emitter = new SseEmitter(SSE_TIMEOUT_MILLIS);
+        emitters.put(phoneNumber, emitter);
+
+        emitter.onCompletion(() -> emitters.remove(phoneNumber));
+        emitter.onTimeout(() -> {
+            emitter.complete();
+            emitters.remove(phoneNumber);
+        });
+        emitter.onError(e -> {
+            emitter.completeWithError(e);
+            emitters.remove(phoneNumber);
+        });
+
+        return emitter;
+    }
+
+    public Optional<SseEmitter> getEmitter(UUID userId) {
+        return Optional.ofNullable(emitters.get(userId));
+    }
+
+    public Collection<SseEmitter> getAllEmitters() {
+        return emitters.values();
+    }
+}
