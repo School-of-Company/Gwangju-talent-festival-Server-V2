ee06f8aa0fc0a575f913129b1142f57141e103a7
박하민 <hamin090403@gmail.com>
2026-03-26 01:26:03 +0900

    add :: SeatReservationCustomRepository 구현

---
+++ src/main/java/team/startup/gwangjutalentfestival/domain/seat/repository/custom/impl/SeatReservationCustomRepositoryImpl.java
@@ -0,0 +1,43 @@
+package team.startup.gwangjutalentfestival.domain.seat.repository.custom.impl;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import team.startup.gwangjutalentfestival.domain.seat.entity.QSeatEntity;
+import team.startup.gwangjutalentfestival.domain.seat.presentation.data.response.SectionSeatNumber;
+import team.startup.gwangjutalentfestival.domain.seat.repository.custom.SeatReservationCustomRepository;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Repository
+@RequiredArgsConstructor
+public class SeatReservationCustomRepositoryImpl implements SeatReservationCustomRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    private static final QSeatEntity seat = QSeatEntity.seatEntity;
+
+    @Override
+    public List<SectionSeatNumber> findAllSeatNumbers() {
+        return queryFactory
+                .select(Projections.constructor(
+                        SectionSeatNumber.class,
+                        seat.seatSection,
+                        seat.seatNumber
+                ))
+                .from(seat)
+                .fetch();
+    }
+
+    @Override
+    public Set<Integer> findSeatNumbersBySeatSection(Character seatSection) {
+        return new HashSet<>(queryFactory
+                .select(seat.seatNumber)
+                .from(seat)
+                .where(seat.seatSection.eq(seatSection))
+                .fetch());
+    }
+}
