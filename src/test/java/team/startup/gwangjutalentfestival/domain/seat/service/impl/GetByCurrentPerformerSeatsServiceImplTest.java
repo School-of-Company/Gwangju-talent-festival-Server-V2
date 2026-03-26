6c4f1b7be762cea7d1774c8e1f7584d8bdb55dc2
박하민 <hamin090403@gmail.com>
2026-03-26 12:04:36 +0900

    update :: UserUtil 주입 받아 사용

--- src/test/java/team/startup/gwangjutalentfestival/domain/seat/service/impl/GetByCurrentPerformerSeatsServiceImplTest.java
+++ src/test/java/team/startup/gwangjutalentfestival/domain/seat/service/impl/GetByCurrentPerformerSeatsServiceImplTest.java
@@ -1,12 +1,9 @@
 package team.startup.gwangjutalentfestival.domain.seat.service.impl;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 import team.startup.gwangjutalentfestival.domain.seat.entity.SeatEntity;
 import team.startup.gwangjutalentfestival.domain.seat.presentation.data.response.GetSeatResponse;
@@ -18,7 +15,6 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mockStatic;
 
 @ExtendWith(MockitoExtension.class)
 class GetByCurrentPerformerSeatsServiceImplTest {
@@ -29,26 +25,16 @@ class GetByCurrentPerformerSeatsServiceImplTest {
     @Mock
     private SeatReservationRepository seatReservationRepository;
 
-    private MockedStatic<UserUtil> userUtilMock;
+    @Mock
+    private UserUtil userUtil;
 
     private static final Long USER_ID = 1L;
-
-    @BeforeEach
-    void setUp() {
-        userUtilMock = mockStatic(UserUtil.class);
-        userUtilMock.when(UserUtil::getCurrentUserId).thenReturn(USER_ID);
-    }
-
-    @AfterEach
-    void tearDown() {
-        userUtilMock.close();
-    }
 
     private UserEntity user() {
         return UserEntity.builder().id(USER_ID).build();
     }
 
-    private SeatEntity seatOf(Character section, Integer number) {
+    private SeatEntity seatOf(String section, Integer number) {
         return SeatEntity.builder()
                 .seatSection(section)
                 .seatNumber(number)
@@ -58,8 +44,9 @@ class GetByCurrentPerformerSeatsServiceImplTest {
 
     @Test
     void 정상_좌석_목록_조회_성공() {
+        given(userUtil.getCurrentUser()).willReturn(user());
         given(seatReservationRepository.findAllByUserId(USER_ID))
-                .willReturn(List.of(seatOf('A', 1), seatOf('B', 3)));
+                .willReturn(List.of(seatOf("A", 1), seatOf("B", 3)));
 
         List<GetSeatResponse> result = getByCurrentPerformerSeatsService.execute();
 
@@ -72,6 +59,7 @@ class GetByCurrentPerformerSeatsServiceImplTest {
 
     @Test
     void 예약된_좌석_없으면_빈_리스트_반환() {
+        given(userUtil.getCurrentUser()).willReturn(user());
         given(seatReservationRepository.findAllByUserId(USER_ID)).willReturn(List.of());
 
         List<GetSeatResponse> result = getByCurrentPerformerSeatsService.execute();
