1a295d606a2ddd8fd8fc4794a21d6303d5918270
C0ng_yun <s25069@gsm.hs.kr>
2026-03-30 20:34:09 +0900

    add :: AsyncConfig 추가

---
+++ src/main/java/team/startup/gwangjutalentfestival/global/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package team.startup.gwangjutalentfestival.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "asyncExecutor")
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("async-executor-");
+        executor.initialize();
+        return executor;
+    }
+}
