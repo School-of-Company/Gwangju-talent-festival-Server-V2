8922eeab21983f1d23af331d02f8d4d3669056f5
C0ng_yun <s25069@gsm.hs.kr>
2026-03-30 20:34:21 +0900

    update :: @EnableAsync 추가

--- src/main/java/team/startup/gwangjutalentfestival/GwangjutalentfestivalApplication.java
+++ src/main/java/team/startup/gwangjutalentfestival/GwangjutalentfestivalApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 import team.startup.gwangjutalentfestival.domain.slogan.properties.SloganSubmissionProperties;
 import team.startup.gwangjutalentfestival.global.jwt.JwtProperties;
 import team.startup.gwangjutalentfestival.global.security.properties.CorsProperties;
@@ -11,6 +12,7 @@ import team.startup.gwangjutalentfestival.global.sms.properties.SmsVerifyPropert
 import team.startup.gwangjutalentfestival.global.sms.properties.SolapiProperties;
 
 @EnableFeignClients
+@EnableAsync
 @EnableConfigurationProperties({JwtProperties.class, CorsProperties.class, SolapiProperties.class, SmsVerifyProperties.class,SloganSubmissionProperties.class})
 @SpringBootApplication
 public class GwangjutalentfestivalApplication {
