52ec477c89d10d5ff04a6fcb14ee92b092f0e881
hamin <hamin090403@gmail.com>
2026-04-05 23:41:31 +0900

    add :: SloganSheetRowData 추가

---
+++ src/main/java/team/startup/gwangjutalentfestival/domain/slogan/presentation/data/SloganSheetRowData.java
@@ -0,0 +1,18 @@
+package team.startup.gwangjutalentfestival.domain.slogan.presentation.data;
+
+public record SloganSheetRowData(
+        String slogan,
+
+        String description,
+
+        String school,
+
+        String name,
+
+        Integer grade,
+
+        Integer classNum,
+
+        String phoneNumber
+) {
+}
