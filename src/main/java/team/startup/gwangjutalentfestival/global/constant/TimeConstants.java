394edf5b56a8796c167abd50819d7fef6c59a6d8
hamin <hamin090403@gmail.com>
2026-04-06 01:45:38 +0900

    add :: TimeConstants

---
+++ src/main/java/team/startup/gwangjutalentfestival/global/constant/TimeConstants.java
@@ -0,0 +1,11 @@
+package team.startup.gwangjutalentfestival.global.constant;
+
+import java.time.ZoneId;
+
+public final class TimeConstants {
+
+    public static final ZoneId SEOUL_ZONE_ID = ZoneId.of("Asia/Seoul");
+
+    private TimeConstants() {
+    }
+}
