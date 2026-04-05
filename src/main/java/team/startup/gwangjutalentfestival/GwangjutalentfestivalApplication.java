fdea60ab4b9a924b388e963381b951530cb3fc65
hamin <hamin090403@gmail.com>
2026-04-06 01:17:51 +0900

    update :: 사용하지 않은 import 제거

--- src/main/java/team/startup/gwangjutalentfestival/GwangjutalentfestivalApplication.java
+++ src/main/java/team/startup/gwangjutalentfestival/GwangjutalentfestivalApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import team.startup.gwangjutalentfestival.domain.slogan.properties.SloganSubmissionProperties;
