590ef371099ecc927c5f2b6f9ecff39795159547
hamin <hamin090403@gmail.com>
2026-04-05 23:40:16 +0900

    add ::  @EnableScheduling 추가

--- src/main/java/team/startup/gwangjutalentfestival/GwangjutalentfestivalApplication.java
+++ src/main/java/team/startup/gwangjutalentfestival/GwangjutalentfestivalApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import team.startup.gwangjutalentfestival.domain.slogan.properties.SloganSubmissionProperties;
 import team.startup.gwangjutalentfestival.global.jwt.JwtProperties;
 import team.startup.gwangjutalentfestival.global.security.properties.CorsProperties;
@@ -13,6 +15,7 @@ import team.startup.gwangjutalentfestival.global.sms.properties.SolapiProperties
 
 @EnableFeignClients
 @EnableAsync
+@EnableScheduling
 @EnableConfigurationProperties({JwtProperties.class, CorsProperties.class, SolapiProperties.class, SmsVerifyProperties.class,SloganSubmissionProperties.class})
 @SpringBootApplication
 public class GwangjutalentfestivalApplication {
