0c4c99614df9fcc259deb4def4126752af3ab8a9
hamin <hamin090403@gmail.com>
2026-04-20 14:44:05 +0900

    docs :: slogan, description JavaDoc 공통 필드로 수정

--- src/main/java/team/startup/gwangjutalentfestival/domain/slogan/presentation/data/SloganSheetRowData.java
+++ src/main/java/team/startup/gwangjutalentfestival/domain/slogan/presentation/data/SloganSheetRowData.java
@@ -5,8 +5,8 @@ import java.time.LocalDate;
 /**
  * Google Sheets에 슬로건 데이터를 추가할 때 사용하는 행 데이터 DTO.
  *
- * @param slogan      슬로건 문구 (ENROLLED 전용)
- * @param description 슬로건 설명 (ENROLLED 전용)
+ * @param slogan      슬로건 문구
+ * @param description 슬로건 설명
  * @param school      학교명 (ENROLLED 전용)
  * @param name        제출자 이름
  * @param grade       학년 (ENROLLED 전용)
