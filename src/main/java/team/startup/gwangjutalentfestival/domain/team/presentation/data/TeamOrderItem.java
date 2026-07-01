627e78defe72a458bfd7114e94ec64da4286f267
jyx-07 <s25069@gsm.hs.kr>
2026-07-01 19:55:58 +0900

    add :: 팀 순서 변경 요청 검증 추가

--- src/main/java/team/startup/gwangjutalentfestival/domain/team/presentation/data/TeamOrderItem.java
+++ src/main/java/team/startup/gwangjutalentfestival/domain/team/presentation/data/TeamOrderItem.java
@@ -1,5 +1,7 @@
 package team.startup.gwangjutalentfestival.domain.team.presentation.data;
 
+import jakarta.validation.constraints.NotNull;
+
 /**
  * 팀 공연 순서 변경을 위한 단일 항목.
  *
@@ -7,7 +9,10 @@ package team.startup.gwangjutalentfestival.domain.team.presentation.data;
  * @param order  변경할 공연 순서 번호
  */
 public record TeamOrderItem(
+        @NotNull(message = "팀 ID를 입력해주세요.")
         Long teamId,
+
+        @NotNull(message = "공연 순서를 입력해주세요.")
         Integer order
 ) {
 }
